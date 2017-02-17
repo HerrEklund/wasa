bba0a87dc888ca8d6d7f83fe7f0dfd4d59660a41
hans <hans.eklund@igw.se>
2017-02-17 11:59:12 +0100

    First commit. Everything at once :).

---
+++ js/wasa_back.js
@@ -0,0 +1,373 @@
+
+/**
+ *
+ * WasaClient, based on webdis
+ *
+ *  https://github.com/nicolasff/webdis
+ *
+ */
+var WasaClient = function (hostname, port, game_id, username, game_event_notification_handler, game_event_handler) {
+    if (typeof jQuery === 'undefined') {
+        throw new Error('Wasa requires jQuery')
+    }
+
+    var that = this;
+
+    that.game_id = game_id;
+    that.username = username;
+    that.http_url = 'http://'+hostname+':'+port;
+    that.ws_url = 'ws://'+hostname+':'+port;
+
+    var wasa_event_channel_name = game_id+'_event_channel';
+    var wasa_event_list_name = game_id+'_event_list';
+
+    subscribe(wasa_event_channel_name, game_event_notification_handler);
+
+    /*
+    var wasaSocket = new WebSocket(this.ws_url+"/");
+
+    wasaSocket.onopen = function() {
+        console.log("JSON socket connected!");
+    };
+    wasaSocket.onmessage = function(messageEvent) {
+        console.log("JSON received:", messageEvent.data);
+    };
+    */
+
+    $.ajaxSetup({
+        error: function (jqXHR, exception) {
+            if (jqXHR.status === 0) {
+                alert('Not connected.\n Verify Network.');
+            } else if (jqXHR.status == 403) {
+                alert('Forbidden. [403]');
+            } else if (jqXHR.status == 404) {
+                alert('Requested page not found. [404]');
+            } else if (jqXHR.status == 500) {
+                alert('Internal Server Error [500].');
+            } else if (exception === 'parsererror') {
+                alert('Requested JSON parse failed.');
+            } else if (exception === 'timeout') {
+                alert('Time out error.');
+            } else if (exception === 'abort') {
+                alert('Ajax request aborted.');
+            } else {
+                alert('Uncaught Error.\n' + jqXHR.responseText);
+            }
+        }
+    });
+
+    that.load_all_events = function(after_events_loaded_callback) {
+        async_get_jsonp('/LRANGE/'+wasa_event_list_name+'/0/-1', function (data) {
+            var events = data['LRANGE'];
+            var eventsLength = events.length;
+
+            for (var i = 0; i < eventsLength; i++) {
+                game_event_handler(JSON.parse(events[i]));
+            }
+
+            // Since async call, let caller continue after all events was loaded
+            after_events_loaded_callback();
+        });
+    };
+
+    that.load_event = function(event_id) {
+        async_get_jsonp('/LINDEX/'+wasa_event_list_name+'/'+event_id, function (data) {
+            var event = data['LINDEX'];
+
+            game_event_handler(JSON.parse(event));
+
+        });
+    };
+
+    that.store_chat_event = function(username, message) {
+        var chat_event = {
+            'event_type': 'chat',
+            'username': encodeURIComponent(username),
+            'time': getTimestamp(),
+            'payload': {
+                'username': encodeURIComponent(username),
+                'message': encodeURIComponent(message)
+            }
+        };
+        store_event_to_list(JSON.stringify(chat_event));
+    };
+
+    that.store_create_component_event = function(tray_component_id, component_id, left, top) {
+        var component_event = {
+            'event_type': 'create_component',
+            'username': encodeURIComponent(username),
+            'time': getTimestamp(),
+            'payload': {
+                'tray_component_id': tray_component_id,
+                'component_id': component_id,
+                'left': left,
+                'top': top
+            }
+        };
+        store_event_to_list(JSON.stringify(component_event));
+    };
+
+    that.store_move_component_event = function(component_id, left, top, zIndex) {
+        var component_event = {
+            'event_type': 'move_component',
+            'username': encodeURIComponent(username),
+            'time': getTimestamp(),
+            'payload': {
+                'component_id': component_id,
+                'left': left,
+                'top': top,
+                'zIndex': zIndex
+            }
+        };
+        store_event_to_list(JSON.stringify(component_event));
+    };
+
+    that.store_delete_component_event = function(component_id) {
+        var component_event = {
+            'event_type': 'delete_component',
+            'username': encodeURIComponent(username),
+            'time': getTimestamp(),
+            'payload': {
+                'component_id': component_id
+            }
+        };
+        store_event_to_list(JSON.stringify(component_event));
+    };
+
+    that.store_rotate_component_event = function(component_id, angle) {
+        var component_event = {
+            'event_type': 'rotate_component',
+            'username': encodeURIComponent(username),
+            'time': getTimestamp(),
+            'payload': {
+                'component_id': component_id,
+                'angle': angle
+            }
+        };
+        store_event_to_list(JSON.stringify(component_event));
+    };
+
+    that.roll_dice_event = function(dice_rolled, result) {
+        var dice_event = {
+            'event_type': 'dice_roll',
+            'username': encodeURIComponent(username),
+            'time': getTimestamp(),
+            'payload': {
+                'dice_rolled': dice_rolled,
+                'result': result
+            }
+        };
+        store_event_to_list(JSON.stringify(dice_event));
+    };
+
+    function store_event_to_list(json_event) {
+        // 1) Store to backend
+        //wasaSocket.send(JSON.stringify(["LPUSH", that.wasa_event_list_name, json_event]));
+
+        console.log("Storing to list "+wasa_event_list_name);
+        async_get_jsonp('/RPUSH/'+wasa_event_list_name+'/'+json_event, function (data) {
+            // Note, list length is returned!
+            console.log("JSONP Callback data: "+ JSON.stringify(data));
+
+            // Publish the index of the event that was stored
+            var event_index = data['RPUSH'];
+            async_get_jsonp('/PUBLISH/' + wasa_event_channel_name + '/'+ event_index);
+        });
+    }
+
+    function getTimestamp() {
+        return Math.floor(Date.now() / 1000);
+    }
+
+    /* Listen to a PUBSUB channel using chunked encoding*/
+    function subscribe(channel_name, game_event_notification_handler) {
+        that.previous_response_length = 0;
+        that.xhr = new XMLHttpRequest();
+        that.xhr.onreadystatechange = on_event_received;
+        that.xhr.open("GET", that.http_url+"/SUBSCRIBE/"+channel_name, true);
+        that.xhr.send(null);
+        that.game_event_notification_handler = game_event_notification_handler;
+    }
+
+    function on_event_received() {
+        console.log("xhr.onreadystatechange");
+        if(that.xhr.readyState == 3)  {
+            var response = that.xhr.responseText;
+            var chunk = response.slice(that.previous_response_length);
+            that.previous_response_length = response.length;
+
+            var pubsub_payload = JSON.parse(chunk);
+
+            console.log("PubSub data received: "+pubsub_payload);
+
+            if (pubsub_payload['SUBSCRIBE'][0] == 'message') {
+                var event_list_length = pubsub_payload['SUBSCRIBE'][2];
+
+                /* Two strategies:
+                    1) Either this is a notification of new events available, clients must then fetch to get up to date
+
+                    2) The message contain the actual event data, so just let the application callback handle it,
+                */
+                //
+
+                // TODO: Load all events that the client has not yet seen
+                // so could be load range or load single here!
+                that.load_event(parseInt(event_list_length) - 1);
+
+                // Forward the event to the Application in case we need to extend this
+                that.game_event_notification_handler(event_list_length);
+            }
+        }
+    }
+
+    /*
+        Ajax JSONP Utility methods
+     */
+    function async_get_jsonp(method, callback) {
+        jQuery.ajax({
+                url: that.http_url + method,
+                method: "GET",
+                dataType: "jsonp",
+                success: function (data) {
+                    if (callback) {
+                        callback(data);
+                    }
+                }
+            }
+        );
+    }
+
+    function async_get_jsonp_parameters(method, parameters, callback) {
+        jQuery.ajax({
+                url: that.http_url + method,
+                method: "GET",
+                data: parameters,
+                dataType: "jsonp",
+                success: function (data) {
+                    callback(data);
+                }
+            }
+        );
+    }
+};
+
+
+/*!
+ * jQuery Cookie Plugin v1.4.1
+ * https://github.com/carhartl/jquery-cookie
+ *
+ * Copyright 2013 Klaus Hartl
+ * Released under the MIT license
+ */
+(function (factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD
+        define(['jquery'], factory);
+    } else if (typeof exports === 'object') {
+        // CommonJS
+        factory(require('jquery'));
+    } else {
+        // Browser globals
+        factory(jQuery);
+    }
+}(function ($) {
+
+    var pluses = /\+/g;
+
+    function encode(s) {
+        return config.raw ? s : encodeURIComponent(s);
+    }
+
+    function decode(s) {
+        return config.raw ? s : decodeURIComponent(s);
+    }
+
+    function stringifyCookieValue(value) {
+        return encode(config.json ? JSON.stringify(value) : String(value));
+    }
+
+    function parseCookieValue(s) {
+        if (s.indexOf('"') === 0) {
+            // This is a quoted cookie as according to RFC2068, unescape...
+            s = s.slice(1, -1).replace(/\\"/g, '"').replace(/\\\\/g, '\\');
+        }
+
+        try {
+            // Replace server-side written pluses with spaces.
+            // If we can't decode the cookie, ignore it, it's unusable.
+            // If we can't parse the cookie, ignore it, it's unusable.
+            s = decodeURIComponent(s.replace(pluses, ' '));
+            return config.json ? JSON.parse(s) : s;
+        } catch (e) {
+        }
+    }
+
+    function read(s, converter) {
+        var value = config.raw ? s : parseCookieValue(s);
+        return $.isFunction(converter) ? converter(value) : value;
+    }
+
+    var config = $.cookie = function (key, value, options) {
+
+        // Write
+
+        if (value !== undefined && !$.isFunction(value)) {
+            options = $.extend({}, config.defaults, options);
+
+            if (typeof options.expires === 'number') {
+                var days = options.expires, t = options.expires = new Date();
+                t.setTime(+t + days * 864e+5);
+            }
+
+            return (document.cookie = [
+                encode(key), '=', stringifyCookieValue(value),
+                options.expires ? '; expires=' + options.expires.toUTCString() : '', // use expires attribute, max-age is not supported by IE
+                options.path ? '; path=' + options.path : '',
+                options.domain ? '; domain=' + options.domain : '',
+                options.secure ? '; secure' : ''
+            ].join(''));
+        }
+
+        // Read
+
+        var result = key ? undefined : {};
+
+        // To prevent the for loop in the first place assign an empty array
+        // in case there are no cookies at all. Also prevents odd result when
+        // calling $.cookie().
+        var cookies = document.cookie ? document.cookie.split('; ') : [];
+
+        for (var i = 0, l = cookies.length; i < l; i++) {
+            var parts = cookies[i].split('=');
+            var name = decode(parts.shift());
+            var cookie = parts.join('=');
+
+            if (key && key === name) {
+                // If second argument (value) is a function it's a converter...
+                result = read(cookie, value);
+                break;
+            }
+
+            // Prevent storing a cookie that we couldn't decode.
+            if (!key && (cookie = read(cookie)) !== undefined) {
+                result[name] = cookie;
+            }
+        }
+
+        return result;
+    };
+
+    config.defaults = {};
+
+    $.removeCookie = function (key, options) {
+        if ($.cookie(key) === undefined) {
+            return false;
+        }
+
+        // Must not alter options, thus extending a fresh object...
+        $.cookie(key, '', $.extend({}, options, {expires: -1}));
+        return !$.cookie(key);
+    };
+
+}));
+
---
+++ js/wasa_front.js
@@ -0,0 +1,480 @@
+
+var component_id = 0;
+var username = 'Guest';
+var game_id = 'foobar';
+
+// Default values
+var animate_transformation = true;
+var animate_duration = 400;
+
+jQuery.fn.rotate = function(degrees) {
+    $(this).css({'transform' : 'rotate('+ degrees +'deg)'});
+    return $(this);
+};
+
+function test_is_touch_device() {
+  return 'ontouchstart' in window;
+}
+
+$( document ).ready(function () {
+    console.log('##');
+    console.log('##');
+    console.log('##  NO CHEATING !!!');
+    console.log('##      (There are anti-cheat triggers that may expose you as a cheater if you fiddle with the internals!)');
+    console.log('##');
+    console.log('##  ... or did you find a bug? Report it to herr.eklund@gmail.com then!');
+    console.log('##');
+    console.log('##');
+
+    var is_touch_device = test_is_touch_device();
+
+    document.getElementById('event_input').onkeypress = function(e){
+        if (!e) e = window.event;
+        var keyCode = e.keyCode || e.which;
+        if (keyCode == '13'){
+          send_chat_message();
+          return false;
+        }
+      };
+
+
+    $(".new_component").draggable({
+        helper: "clone",
+        appendTo: "#game_board",
+    });
+    $(".component_tray").droppable({
+        accept: '.component',
+        drop: function(event, ui) {
+            wasa_client.store_delete_component_event($(ui.helper).attr('id'));
+        }
+    });
+    $("#game_board").droppable({
+        accept: '.new_component',
+        drop: function(event, ui) {
+            var ghost_component = $(ui.helper);
+
+            var new_component = ghost_component.clone(true);
+
+            // Extract what we need to post the event
+            var tray_component_id = new_component.context.id;
+            var coordinates = ghost_component.position();
+
+            var left = coordinates.left;
+            var top = coordinates.top;
+
+            // Store event to backend
+            wasa_client.store_create_component_event(tray_component_id, get_random_id(), left, top);
+
+            ghost_component.remove();
+        }
+    });
+
+    $("body").click(function () {
+        resetSelectionAndLineup();
+    });
+
+    /**
+     * TODO: investigate how to enable lasso properly, not working well atm
+     *
+    if (!is_touch_device) {
+        $("#game_board").selectable({
+          filter: "div",
+            start: function( event, ui ) {
+
+                resetSelectionAndLineup();
+            },
+          selected: function( event, ui ) {
+              var selected_element = $(ui)[0];
+              var selected_id = selected_element.selected.id;
+              if (selected_id) {
+                  console.log("Selected "+selected_id);
+                  $('#'+selected_id).addClass('selected_component');
+              } else {
+                  console.log("Select event with not hit.")
+              }
+          }
+        });
+    }
+     */
+});
+
+/**
+ *
+ * Application event handlers
+ *
+ */
+function addChatMessage(chat_message_event) {
+    var e = chat_message_event;
+
+    var username = e['payload']['username'];
+    var message = e['payload']['message'];
+
+    var events_textarea = $('#events_textarea');
+    events_textarea.append("\n"+username+": "+ message);
+
+    events_textarea.scrollTop(events_textarea[0].scrollHeight);
+}
+
+
+/**
+ *
+ * Game event handlers
+ *
+ */
+function createAndAddComponentToGameBoard(create_component_event) {
+    /**
+     * Will be called when events are received from back end.
+     *
+     */
+    var e = create_component_event;
+    var event_username = e['username'];
+    var time = e['time'];
+
+    // Grab the tray component
+    var tray_component = $('#'+e['payload']['tray_component_id']);
+    var new_component_id = e['payload']['component_id'];
+
+    // Create component
+    var new_component = tray_component.clone();
+
+    addComponentToGameBoard(new_component, new_component_id, e['payload']['top'], e['payload']['left']);
+
+    print_game_event(time, event_username, "Created component "+new_component_id);
+}
+
+function moveComponent(move_component_event) {
+    var e = move_component_event;
+    var event_username = e['username'];
+    var time = e['time'];
+
+    var component_id = e['payload']['component_id'];
+    var left = e['payload']['left'];
+    var top = e['payload']['top'];
+    var zIndex = e['payload']['zIndex'];
+
+    var transformation_css = {top: top, left: left, zIndex: zIndex};
+    if (animate_transformation) {
+        $('#'+component_id).animate(transformation_css, animate_duration);
+    } else {
+        $('#'+component_id).css(transformation_css);
+    }
+
+    print_game_event(time, event_username, "Moved component "+component_id);
+}
+
+function deleteComponent(delete_component_event) {
+    var e = delete_component_event;
+    var event_username = e['username'];
+    var time = e['time'];
+
+    var component_id = e['payload']['component_id'];
+    $('#'+component_id).remove();
+
+    print_game_event(time, event_username, "Removed component "+component_id);
+}
+
+function rotateComponent(rotate_component_event) {
+    var e = rotate_component_event;
+    var event_username = e['username'];
+    var time = e['time'];
+
+    var component_id = e['payload']['component_id'];
+    var new_angle = e['payload']['angle'];
+
+    var component = $('#'+component_id);
+
+    component.attr("rotation", new_angle);
+
+    // Do this when event is received from back-end
+    rotate(component, new_angle);
+
+    print_game_event(time, event_username, "Rotated component "+component_id);
+}
+
+function handleDiceRollEvent(dice_roll_event) {
+    var e = dice_roll_event;
+    var event_username = e['username'];
+    var time = e['time'];
+
+    var dice_rolled = e['payload']['dice_rolled'];
+    var result = e['payload']['result'];
+
+    print_game_event(time, event_username, dice_rolled + " : " + result);
+}
+
+/**
+ *
+ *
+ *  GUI Support functions
+ *
+ */
+function addComponentToGameBoard(new_component, new_component_id, top, left) {
+
+    new_component.appendTo('#game_board');
+    new_component.attr('id', new_component_id);
+
+    new_component.css({top: top, left: left, position:'absolute'});
+
+    new_component.removeClass("new_component");
+
+    new_component.draggable({
+        start: function (event, ui) {
+            // Will move selected component to top when touched
+            var topZ = 0;
+            var topZ_element;
+            $('.component').each(function () {
+                var thisZ = parseInt($(this).css('zIndex'), 10);
+                if (thisZ > topZ) {
+                    topZ = thisZ;
+                    topZ_element = this;
+                }
+            });
+            $(event.target).css('zIndex', topZ + 1);
+        },
+        stop: function(event, ui) {
+            var coordinates = $(event.target).position();
+            var left = coordinates.left;
+            var top = coordinates.top;
+            var zIndex = $(event.target).css('zIndex');
+
+            wasa_client.store_move_component_event($(event.target).context.id, left, top, zIndex);
+
+            // Also move all selected
+            /*
+            $('.selected_component').each( function (index, value) {
+                var coordinates = $(value).position();
+                var left = coordinates.left;
+                var top = coordinates.top;
+                var zIndex = $(value).css('zIndex');
+
+                wasa_client.store_move_component_event($(value).context.id, left, top, zIndex);
+            })
+            */
+        }
+    });
+
+    new_component.attr("rotation", 0);
+
+    if (enable_component_rotation) {
+        new_component.click(function (event, ui) {
+            var new_angle;
+            var current_rotation = new_component.attr("rotation");
+
+            if (isNaN(current_rotation)) {
+                current_rotation = 0;
+            }
+
+            if (event.shiftKey) {
+                new_angle = parseInt(current_rotation) - 9;
+            } else {
+                new_angle = parseInt(current_rotation) + 9;
+            }
+            if (new_angle > 360) {
+                new_angle = new_angle - 360;
+            }
+
+            // TODO: Event
+            wasa_client.store_rotate_component_event(new_component[0].id, new_angle);
+        });
+    }
+
+    var DELAY = 300, clicks = 0, timer = null;
+    if (enable_stack_selection) {
+
+        new_component.on("click", function(e){
+
+            clicks++;  //count clicks
+
+            if(clicks === 1) {
+                console.log("click on "+new_component_id);
+
+                timer = setTimeout(function() {
+                    if (new_component.hasClass('selected_component')) {
+                        new_component.removeClass('selected_component');
+                    } else {
+                        resetLineup();
+                        new_component.addClass('selected_component');
+                    }
+
+                    clicks = 0;             //after action performed, reset counter
+
+                }, DELAY, new_component_id);
+
+            } else {
+                // Custom Double click handler
+                console.log("duble click on "+new_component_id);
+
+                var position = new_component.position();
+
+                // Select all that share a coordiante with clicked element
+                var added_to_lineup = false;
+
+                // IF dbl clicked on selected, add all seleted to lineup
+                if(new_component.hasClass('selected_component')) {
+
+                    resetLineup();
+                    $('.selected_component').each(function () {
+                        addCloneToLineupBox($(this));
+                        added_to_lineup = true;
+                    });
+                } else {
+                    $('.component').each(function () {
+                        if( overlaps(new_component, $(this))) {
+                            $(this).addClass('selected_component')
+                        }
+                    });
+
+                }
+                if (added_to_lineup) {
+                    //addCloneToLineupBox($(e.target));
+
+                    // Lineup was added to, add clicked target to lineup also and move lineup to above clicked target
+                    placeLineupBox(position.left, position.top);
+                }
+
+                clearTimeout(timer);    //prevent single-click action
+                clicks = 0;             //after action performed, reset counter
+            }
+            e.stopPropagation();
+
+
+        })
+        .on("dblclick", function(e){
+            e.preventDefault();  //cancel system double-click event
+        });
+    }
+
+    new_component.dblclick(function (event, ui) {
+        // Warning, it is not advised to both use a click AND dblclick handler
+    });
+
+    new_component.prop('title', 'ID='+new_component_id);
+}
+
+function resetSelectionAndLineup() {
+    console.log("resetSelectionAndLineup()");
+
+    $('.selected_component').removeClass('selected_component');
+
+    resetLineup();
+}
+
+function resetLineup() {
+     console.log("resetLineup()");
+
+   // Empty lineup_box and show temp hidden
+    $('.lineup_component').remove();
+
+    $('.hidden_component').draggable( 'enable' );
+    $('.hidden_component').removeClass('hidden_component');
+
+    $('#lineup_box').removeClass('lineup_box_active');
+}
+
+function addCloneToLineupBox(component) {
+    var lineup_clone = component.clone(true);
+    lineup_clone.css({position:'static'});
+    lineup_clone.removeClass('selected_component');
+    lineup_clone.addClass('lineup_component');
+
+    lineup_clone.appendTo('#lineup_box');
+
+    lineup_clone.draggable( 'disable' );
+
+    // Hide original
+    component.addClass('hidden_component');
+    $('#lineup_box').addClass('lineup_box_active');
+}
+
+function placeLineupBox(left, top) {
+  $('#lineup_box').css({top: top, left: left, position:'absolute'});
+}
+
+
+var overlaps = (function () {
+    function getPositions( elem ) {
+        var pos, width, height;
+        pos = $( elem ).position();
+        width = $( elem ).width();
+        height = $( elem ).height();
+        return [ [ pos.left, pos.left + width ], [ pos.top, pos.top + height ] ];
+    }
+
+    function comparePositions( p1, p2 ) {
+        var r1, r2;
+        r1 = p1[0] < p2[0] ? p1 : p2;
+        r2 = p1[0] < p2[0] ? p2 : p1;
+        return r1[1] > r2[0] || r1[0] === r2[0];
+    }
+
+    return function ( a, b ) {
+        var pos1 = getPositions( a ),
+            pos2 = getPositions( b );
+        return comparePositions( pos1[0], pos2[0] ) && comparePositions( pos1[1], pos2[1] );
+    };
+})();
+
+function rotate($el, degrees) {
+    var transformation_css = {
+      '-webkit-transform' : 'rotate('+degrees+'deg)',
+         '-moz-transform' : 'rotate('+degrees+'deg)',
+          '-ms-transform' : 'rotate('+degrees+'deg)',
+           '-o-transform' : 'rotate('+degrees+'deg)',
+              'transform' : 'rotate('+degrees+'deg)',
+                   'zoom' : 1
+
+    };
+
+    if(animate_transformation) {
+        // TODO: cant animate rotations yet
+        $el.css(transformation_css, animate_duration);
+    } else {
+        $el.css(transformation_css);
+    }
+}
+
+function get_random_id() {
+    return Math.random().toString(36).substring(3, 11);
+}
+
+function print_game_event(unix_timestamp, event_username, text) {
+    var events_textarea = $('#events_textarea');
+
+    var ptime = get_printable_time(unix_timestamp);
+
+    events_textarea.append("\n"+ ptime + " ["+ event_username + "] : " + text);
+
+    events_textarea.scrollTop(events_textarea[0].scrollHeight);
+}
+
+function get_printable_time(unix_timestamp) {
+  var a = new Date(unix_timestamp * 1000);
+  var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+  var year = a.getFullYear();
+  var month = months[a.getMonth()];
+  var date = a.getDate();
+  var hour = a.getHours() < 10 ? '0' + a.getHours() : a.getHours();
+  var min = a.getMinutes() < 10 ? '0' + a.getMinutes() : a.getMinutes();
+  var sec = a.getSeconds() < 10 ? '0' + a.getSeconds() : a.getSeconds();
+  //var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec ;
+  var time = hour + ':' + min + ':' + sec ;
+  return time;
+}
+
+function send_chat_message() {
+    var message = $('#event_input').val();
+    wasa_client.store_chat_event(username, message);
+    $('#event_input').val('');
+}
+
+function roll_D6() {
+    var result = Math.floor(Math.random() * 6) + 1;
+    wasa_client.roll_dice_event('1D6', result);
+}
+
+function roll_2D6() {
+    var d1 = Math.floor(Math.random() * 6) + 1;
+    var d2 = Math.floor(Math.random() * 6) + 1;
+    var result = d1+d2;
+    wasa_client.roll_dice_event('2D6', result);
+}
